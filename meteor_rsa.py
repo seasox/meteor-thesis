1ed881806209cfcee551c879f71cfbcba19c77c4
Jeremy Boy <github@jboy.eu>
2022-04-24 19:57:02 +0200

    def main, add some (commented out) RsaStego stuff which doesn't work

--- meteor_rsa.py
+++ meteor_rsa.py
@@ -1,82 +1,113 @@
+import os
+import random
+from typing import Tuple
+
+import bitarray
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 
-from coder import MeteorCoder
+import meteor_symmetric
+from coder import MeteorCoder, DRBG
 import cryptography.hazmat.primitives.asymmetric.rsa as rsa
 
-chosen_context = "Despite a long history of research and wide-spread applications to censorship resistant systems, practical steganographic systems capable of embedding messages into realistic communication distributions, like text, do not exist.\n\n"
+from util import get_model
 
-message_text = "sample text"
+"""
+class RsaStegoCrypto:
+    def __init__(self):
+        pass
 
-# Use gpt2-medium for 345M param model
-# Use gpt2-large for 774M param model
-def get_model(seed=1234, model_name='gpt2', device='cuda'):
-    import numpy as np
-    import torch
-    from pytorch_transformers import GPT2LMHeadModel, GPT2Tokenizer
-    np.random.seed(seed)
-    torch.random.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
+    def arr_xor(self, a, b):
+        out = a.copy()
+        for i in range(len(out)):
+            out = out[i] ^ b[i]
+        return out
 
-    enc = GPT2Tokenizer.from_pretrained(model_name)
-    enc.unk_token = None
-    enc.bos_token = None
-    enc.eos_token = None
-    
-    model = GPT2LMHeadModel.from_pretrained(model_name)
-    model.to(device)
-    model.eval()
-    # model.double()  # want to avoid using this
+    def encrypt(self, m: bytes, N: long, e: long):
+        k = N.bit_length()
+        l = len(m)
+        x = [0] * (l+1)
+        b = [0] * (l+1)
+        while True:
+            x[0] = random.randint(1, N)  # top exclusive
+            for i in range(1, l+1):
+                b[i] = x[i-1] % 2
+                x[i] = pow(x[i-1], e, N)
+            c = numpy.random.randint(0, 2)
+            if x[l] <= (2**k-N) or c == 1:
+                break
+        xd = None
+        if (x[1] <= 2 ** k - N) and c == 0:
+            xd = x
+        elif (x[1] <= 2 ** k - N) and c == 1:
+            xd = 2 ** k - x
+        assert xd is not None
 
-    def decode(self, token_ids, **kwargs):
-        filtered_tokens = self.convert_ids_to_tokens(token_ids)
-        text = self.convert_tokens_to_string(filtered_tokens)
-        return text
-    GPT2Tokenizer.decode = decode
-
-    def _convert_token_to_id(self, token):
-        return self.encoder.get(token, 0)
-    GPT2Tokenizer._convert_token_to_id = _convert_token_to_id
-
-    return enc, model
-
-
-model_name = 'gpt2-medium'
-device = 'cpu'
-
-enc, model = get_model(model_name=model_name, device=device)
-    
-    
-coder = MeteorCoder(enc, model, device)
+        return xd, self.arr_xor(m, b)
 
 
-# generate a private key pair
-bob_private_key = rsa.generate_private_key(65537, 2048)
+    def decrypt(self, xd, c, N, d):
+        l = len(c)
+        k = N.bit_length()
+        x = [0] * (l+1)
+        if xd > N:
+            x[l] = xd
+        else:
+            x[l] = 2 ** k - xd
 
-# we expect this public key to be available to Alice
-bob_public_key = bob_private_key.public_key()
+        b = [None] * l
+        for i in range(l, 0, -1): #  bottom exclusive
+            x[i-1] = pow(x[i], d, N)
+            b[i] = x[i-1] % 2
 
-# apply OAEP padding
-pad = padding.OAEP(
-        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-        algorithm=hashes.SHA256(),
-        label=None)
+        return self.arr_xor(c, b)
+"""
 
-# encrypt message using Bob's public key
-encrypted_message: bytes = bob_public_key.encrypt(message_text.encode('UTF-8'), pad)
+if __name__ == '__main__':
+    chosen_context_str = "Despite a long history of research and wide-spread applications to censorship resistant systems, practical steganographic systems capable of embedding messages into realistic communication distributions, like text, do not exist.\n\n"
 
-enc_text = encrypted_message.decode("ASCII", 'ignore')
+    message_text = "sample text"
 
-# encrypted_message probably is not uniformly distributed and might leak information to Warden
+    model_name = 'gpt2-medium'
+    device = 'cpu'
 
-# send encrypted_message to Bob
+    enc, model = get_model(model_name=model_name, device=device)
 
-key = b'\00'*64
-nonce = b'\x01'*64
+    chosen_context = enc.encode(chosen_context_str)
 
-x = coder.encode_message(enc_text, chosen_context, key, nonce)
-recovered_encrypted_message = coder.decode_message(x[0], chosen_context, key, nonce)
 
-decrypted_message = bob_private_key.decrypt(recovered_encrypted_message, pad)
+    coder = MeteorCoder(enc, model, device)
 
-assert decrypted_message == message_text
+
+    # generate a private key pair
+    bob_private_key = rsa.generate_private_key(65537, 2048)
+
+    # we expect this public key to be available to Alice
+    bob_public_key = bob_private_key.public_key()
+
+    # apply OAEP padding
+    pad = padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None)
+
+    # encrypt message using Bob's public key
+    encrypted_message: bytes = bob_public_key.encrypt(message_text.encode('UTF-8'), pad)
+    encrypted_message_ba = bitarray.bitarray()
+    encrypted_message_ba.frombytes(encrypted_message)
+
+    # encrypted_message probably is not uniformly distributed and might leak information to Warden
+
+    # send encrypted_message to Bob
+
+    sample_seed_prefix = b'sample'
+    sample_key = b'0x01' * 64
+    sample_nonce = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    encryption = meteor_symmetric.PRGEncryption(DRBG(sample_key, sample_seed_prefix + sample_nonce))
+    decryption = meteor_symmetric.PRGEncryption(DRBG(sample_key, sample_seed_prefix + sample_nonce))
+    x = coder.encode_binary(encrypted_message_ba.tolist(), chosen_context, encryption)
+    recovered_encrypted_message = coder.decode_binary(x[0], chosen_context, decryption)
+
+    decrypted_message = bob_private_key.decrypt(bitarray.bitarray(recovered_encrypted_message).tobytes()[:256], pad).decode('UTF-8')
+
+    assert decrypted_message == message_text
