4f5782d66de3c87b7a3a40854c18475863763cb8
Jeremy Boy <github@jboy.eu>
2022-06-27 15:50:58 +0200

    mv mismatches.pickle meteor_statistics.pickle

--- code/meteor_statistics.py
+++ code/meteor_statistics.py
@@ -9,7 +9,7 @@ from coder import MeteorCoder
 
 def write_mismatches(mismatches):
     print("write mismatches...")
-    f = open("mismatches.pickle", "wb+")
+    f = open("meteor_statistics.pickle", "wb+")
     pickle.dump(mismatches, f)
     f.close()
     print("done")
@@ -18,8 +18,8 @@ def write_mismatches(mismatches):
 def load_mismatches():
     print("load mismatches...")
     from os.path import exists
-    if exists("mismatches.pickle"):
-        f = open("mismatches.pickle", "rb")
+    if exists("meteor_statistics.pickle"):
+        f = open("meteor_statistics.pickle", "rb")
         o = pickle.load(f)
         f.close()
         return o
