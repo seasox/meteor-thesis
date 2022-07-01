280e703f7160e2c37482a3088807f6915639418f
Jeremy Boy <github@jboy.eu>
2022-07-01 08:17:47 +0200

    add naive implementation for tokenize_candidates

--- code/util.py
+++ code/util.py
@@ -34,4 +34,41 @@ def get_model(seed=1234, model_name='gpt2', device='cuda'):
 
     GPT2Tokenizer._convert_token_to_id = _convert_token_to_id
 
+    tokenize_cache = {}
+
+    def tokenize_candidates(self, parent, start=True): # parent: Union[str, Node]
+        from anytree import Node
+        import sys
+        if isinstance(parent, str):
+            # apply byte-encoding to text
+            # this works for python 3 only
+            if sys.version_info[0] == 2:
+                text = ''.join(self.byte_encoder[ord(b)] for b in parent)
+            else:
+                text = ''.join([self.byte_encoder[b] for b in parent.encode('utf-8')])
+            parent = Node(None, parent=None, text=text)
+        if parent.text == '':
+            return parent
+        print("text: " + parent.text)
+        for token, id in self.encoder.items():
+            if parent.__dict__['text'].startswith(token):
+                self.tokenize_candidates(Node(token, parent=parent, text=parent.__dict__['text'][len(token):]))
+        return parent
+    GPT2Tokenizer.tokenize_candidates = tokenize_candidates
+
     return enc, model
+
+
+if __name__ == '__main__':
+    text = """
+    "Steganography", "breaking the pen", "to embed text in a message", "hot spread", "until its cover is removed", or whatever, has a long history among computer scientists celebrated by publications like Hunt Eilers' Frauenkumme[4] and that of Dr. Ralf Corroon in particular. Or the respected Peter Iversen[5] who has been working on Steganography and NetWrappers since 1989[6] and you could say that it's been "tovert[ing] for years".
+
+    Armed Upon Me![7] was an excellent article about Parrot Steganography system and its derivatives that has good graphics what to be original and features between the quoted text and the "hidden" image-fiche and snapshot files specially written for peer review, but also Steve Fox who has released allowed full text of both elements[8]
+
+    The term "Steganography"(stype "e") is derived from the German vs in Roman Meaning with an AI"""
+    text = 'hello world'
+    from anytree import RenderTree
+
+    enc, model = get_model(device='cpu')
+    tree = enc.tokenize_candidates(text)
+    print(RenderTree(tree))
