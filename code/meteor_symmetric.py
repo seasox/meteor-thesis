0dff5dc90763524d1793ad500cb792167c316543
Jeremy Boy <github@jboy.eu>
2022-08-17 13:36:25 +0200

    update meteor_symmetric for failed decoding PoC using arithmetic coding

--- code/meteor_symmetric.py
+++ code/meteor_symmetric.py
@@ -19,20 +19,26 @@ def main():
     key = b'0x01' * 64
     nonce = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
     chosen_context = "Despite a long history of research and wide-spread applications to censorship resistant systems, practical steganographic systems capable of embedding messages into realistic communication distributions, like text, do not exist.\n\n"
-    message_text = "Hi! Did anyone follow you last night? Are we still up for tommorow? It was 12 am at the market, right?"
+    message_text = "Hello world"
     while True:
+        key = b'\xb9^\x03\xa1\xd0\x1b0O\x11\xdc\xf2\xbc\x84N_\xd3\xcb\xedA%;\x05\x06\x87`\x04 {,*\x10\xe2\xd8\x9c\x1a@\xe950\xbf\xcf\xaa\xeeT\xe6j\xe0H\xd2\xd2\xa56a[\n\x81\xaf\xe7\x92\x888w\xd5\xb6'
         # key = os.urandom(64)
         # nonce = os.urandom(64)
         import time
         start = time.time()
-        x = coder.encode_message(message_text, chosen_context, key, nonce)
+        x, enc_tokens, stats = coder.encode_message(message_text, chosen_context, key, nonce, coding='arithmetic')
         end = time.time()
         print("Encode took {:.02f} s".format(end - start))
         start = time.time()
-        y = coder.decode_message(x[0], chosen_context, key, nonce)
+        y, dec_tokens = coder.decode_message(x, chosen_context, key, nonce, coding='arithmetic')
         end = time.time()
         print("Decode took {:.02f} s".format(end - start))
-        assert y[0] == message_text
+        print(y)
+        if y[0] != message_text[0]:
+            print(key)
+            print(enc_tokens)
+            print(dec_tokens)
+            assert False
 
 
 if __name__ == '__main__':
