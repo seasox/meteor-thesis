b161c418a59c5d84e371a0cb1a934ce42d9b5acb
Jeremy Boy <github@jboy.eu>
2022-08-18 15:51:27 +0200

    update visualize_stats for plot latex export

--- code/visualize_stats.py
+++ code/visualize_stats.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -14,18 +15,19 @@ if __name__ == '__main__':
     fname = 'meteor_statistics.pickle'
     data = ld(fname)
 
-    import matplotlib.pyplot as plt
-    import matplotlib.ticker as plticker
 
     plt.figure(dpi=1200)
-    for coding in ['arithmetic', 'utf-8']:
+    for coding in ['arithmetic']:
         print(f'generating {coding} stats')
         data_w_coding = list(filter(lambda x: x.coding == coding, data))
+        if not data_w_coding:
+            continue
         # print(data)
         mismatched_codings = list(filter(lambda x: len(x.mismatches) > 0, data_w_coding))
         encoded_tokens = list(map(lambda x: len(x.encoded_tokens), data_w_coding))
         mismatch_count = list(map(lambda x: len(x.mismatches), data_w_coding))
-        mismatch_rate = list(map(lambda x: x[0] / x[1] if x[1] != 0 else x[0], zip(encoded_tokens, mismatch_count)))
+        mismatch_rate = list(filter(lambda x: x != float('inf'), map(lambda x: x[0] / x[1] if x[1] != 0 else 0,
+                                                                     zip(encoded_tokens, mismatch_count))))
         avg_mismatch_rate = [sum(encoded_tokens) / sum(mismatch_count) for _ in range(len(encoded_tokens))]
         # avg_mismatch_rate = [np.mean(mismatch_stat[:i]) if i > 0 else 0 for i in range(len(mismatch_stat))]
         bits_per_word = list(flat_map(lambda x: x.stats['encoded_bits_in_output'], data_w_coding))
@@ -35,25 +37,47 @@ if __name__ == '__main__':
         kl = list(map(lambda x: 1 / x.stats['kl'], data_w_coding))
         ppl = list(map(lambda x: 1 / x.stats['ppl'], data_w_coding))
 
-        fig, axs = plt.subplots(4)
-        axs[0].set_title("bits per word")
-        axs[0].hist(bits_per_word, bins=np.arange(max(bits_per_word)) - 0.5, ec="k")
+        # fig, axs = plt.subplots(4)
+        # axs[0].set_title("bits per word")
+        # axs[0].hist(bits_per_word, bins=np.arange(max(bits_per_word)) - 0.5, ec="k")
         # axs[0].plot(avg_bits_per_word, label="avg bits per word")
-        axs[1].set_title("mismatched decodings")
-        loc = plticker.MultipleLocator(base=1.0)  # this locator puts ticks at regular intervals
-        axs[1].yaxis.set_major_locator(loc)
-        axs[1].plot(mismatch_count)
+        # axs[1].set_title("mismatched decodings")
+        # loc = plticker.MultipleLocator(base=1.0)  # this locator puts ticks at regular intervals
+        # axs[1].yaxis.set_major_locator(loc)
+        # axs[1].xaxis.set_visible(False)
+        # axs[1].plot(mismatch_count, '.')
         # axs[1].plot(avg_mismatch_rate, label="avg mismatch rate")
-        axs[2].set_title("mismatch rate (#encoded/#mismatch)")
-        axs[2].plot(mismatch_rate, label="mismatch rate (#mismatch/#encoded)")
-        axs[2].annotate('%E' % avg_mismatch_rate[-1], (len(avg_mismatch_rate), avg_mismatch_rate[-1]))
-        axs[2].plot(avg_mismatch_rate, label="avg mismatch rate")
-        axs[3].set_title('encoded tokens')
-        axs[3].plot(encoded_tokens, label='encoded tokens')
-        fig.suptitle(f'{coding} (total: {sum(encoded_tokens)} tokens)')
-        fig.tight_layout()
-        # plt.legend()
+        # axs[2].set_title("mismatch rate (no. encoded/no. mismatch)")
+        # axs[2].xaxis.set_visible(False)
+        # axs[2].plot(mismatch_rate, '.', label="mismatch rate (no. mismatch/no. encoded)")
+        # axs[2].annotate('%E' % avg_mismatch_rate[-1], (len(avg_mismatch_rate), avg_mismatch_rate[-1]))
+        # axs[2].plot(avg_mismatch_rate, label="avg mismatch rate")
+        # axs[3].set_title('encoded tokens')
+        # axs[3].xaxis.set_visible(False)
+        # axs[3].plot(encoded_tokens, '.', label='encoded tokens')
+        # fig.suptitle(f'{coding} (total: {sum(encoded_tokens)} tokens)')
+        # fig.tight_layout()
+
+        # plt.set_title("mismatch rate (no. encoded/no. mismatch)")
+
+        # plt.yscale('log')
+        histbins = np.arange(min(mismatch_count) - 0.5, max(mismatch_count) + 1.5)
+        plt.xlim(min(histbins), max(histbins))
+        plt.hist(mismatch_count, bins=histbins, label="mismatch count", align='mid')
+        plt.tight_layout()
+        plt.legend()
         plt.show()
+        # tikzplotlib.save(f'../tex/fig_meteor_stats_mismatch_count_{coding}.tikz')
+        plt.clf()
+
+        # plt.annotate('%E' % avg_mismatch_rate[-1], (len(avg_mismatch_rate), avg_mismatch_rate[-1]))
+        # plt.plot(avg_mismatch_rate, label="avg mismatch rate")
+        # xaxis = plt.gca()
+        # xaxis.set_visible(False)
+        plt.hist(encoded_tokens, bins=np.arange(min(encoded_tokens) - 0.5, max(encoded_tokens) + 1.5),
+                 label='encoded tokens')
+        plt.tight_layout()
+        plt.legend()
+        plt.show()
+        # tikzplotlib.save(f'../tex/fig_meteor_stats_encoded_tokens_{coding}.tikz')
 # plt.savefig('plot.eps', format='eps')
-# import tikzplotlib
-# tikzplotlib.save("plot.tex")
