6f4f399458f468295f061c031a44a8063bd51663
Jeremy Boy <github@jboy.eu>
2022-08-18 15:51:01 +0200

    repeatedly encode hamlet using random context

--- code/meteor_statistics.py
+++ code/meteor_statistics.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import pickle
+import random
 
 from coder import MeteorCoder
 from meteor_analysis import compare_tokens
@@ -41,8 +42,7 @@ def main():
     # Constants for HMAC-DRBG -- MUST CHANGE FOR SECURE IMPLEMENTATION
     chosen_context = "Despite a long history of research and wide-spread applications to censorship resistant systems, practical steganographic systems capable of embedding messages into realistic communication distributions, like text, do not exist.\n\n"
     # message_text = "Hi! Did anyone follow you last night? Are we still up for tommorow? It was 12 am at the market, right?"
-    import coder as codr
-    context_tokens = codr.encode_context(chosen_context, enc)
+    # context_tokens = codr.encode_context(chosen_context, enc)
     message_text = open('hamlet_act1.txt', 'r').read()
     """if True:
         message_text = open('hamlet_act1.txt', 'r').read()
@@ -60,40 +60,45 @@ def main():
         hamlet_f.close()"""
 
     comparisons = load_mismatches()
-    i = 0
     hamlet_len = len(message_text)
-    step_size = 128
-    while i < hamlet_len:
-        coding = 'utf-8'
-        key = os.urandom(64)
-        nonce = os.urandom(64)
-        import time
-        start = time.time()
-        text, enc_toks, stats = coder.encode_message(message_text[i:i + step_size], chosen_context, key, nonce,
-                                                     coding=coding)
-        end = time.time()
-        print("Encode took {:.02f} s".format(end - start))
-        start = time.time()
-        # y = coder.decode_message(x[0], chosen_context, key, nonce)
-        dec_toks = enc.tokenize(text)
-        end = time.time()
-        print("Decode took {:.02f} s".format(end - start))
-        num_encoded_tokens = len(enc_toks)
-        num_decoded_tokens = len(dec_toks)
-        # log comparison statistics
-        comparison = compare_tokens(message_text[i:i + step_size].encode('utf-8'), chosen_context, key, nonce, coding,
-                                    enc_toks, dec_toks, stats)
-        comparisons += [comparison]
-        num_mismatch = len(comparison.mismatches)
-        write_mismatches(comparisons)
-        print(comparison)
-        print("encode: ", enc_toks)
-        print("decode: ", dec_toks)
+    step_size = 1024
+    for coding in ['arithmetic']:
+        for j in range(1, 20):
+            i = 0
+            while i < hamlet_len:
+                key = os.urandom(64)
+                nonce = os.urandom(64)
+                import time
+                start = time.time()
+                context_start_idx = random.randrange(0, hamlet_len - 128)
+                context_str = message_text[context_start_idx: context_start_idx + 128] + '\n\n'
+                text, enc_toks, stats = coder.encode_message(message_text[i:i + step_size], context_str, key, nonce,
+                                                             coding=coding)
+                end = time.time()
+                print("Encode took {:.02f} s".format(end - start))
+                start = time.time()
+                # y = coder.decode_message(x[0], chosen_context, key, nonce)
+                dec_toks = enc.tokenize(text)
+                end = time.time()
+                print("Decode took {:.02f} s".format(end - start))
+                num_encoded_tokens = len(enc_toks)
+                num_decoded_tokens = len(dec_toks)
+                # log comparison statistics
+                comparison = compare_tokens(message_text[i:i + step_size].encode('utf-8'), chosen_context, key, nonce,
+                                            coding,
+                                            enc_toks, dec_toks, stats)
+                comparisons += [comparison]
+                num_mismatch = len(comparison.mismatches)
+                write_mismatches(comparisons)
+                print(comparison)
+                print("encode: ", enc_toks)
+                print("decode: ", dec_toks)
 
-        print("mismatches = ", num_mismatch)
-        print("encoded tokens per mismatch = ", num_encoded_tokens / num_mismatch if num_mismatch > 0 else 0)
-        print("decoded tokens per mismatch = ", num_decoded_tokens / num_mismatch if num_mismatch > 0 else 0)
-        i += step_size
+                i += step_size
+                print("mismatches = ", num_mismatch)
+                print("encoded tokens per mismatch = ", num_encoded_tokens / num_mismatch if num_mismatch > 0 else 0)
+                print("decoded tokens per mismatch = ", num_decoded_tokens / num_mismatch if num_mismatch > 0 else 0)
+                print("total progress = ", ((j * hamlet_len + i) / (20 * hamlet_len)))
 
 
 if __name__ == '__main__':
