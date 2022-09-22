6e185b3cd410d60b2204c40d243fe43ef8392671
Jeremy Boy <github@jboy.eu>
2022-09-22 14:15:24 +0200

    refactor

--- code/visualize_stats.py
+++ code/visualize_stats.py
@@ -3,6 +3,10 @@ import numpy as np
 import tikzplotlib
 
 
+def flat_map(f, xs):
+    return [y for ys in xs for y in f(ys)]
+
+
 def ld(fname):
     f = open(fname, "rb")
     import pickle
@@ -11,73 +15,43 @@ def ld(fname):
     return o
 
 
-if __name__ == '__main__':
-    def flat_map(f, xs):
-        return [y for ys in xs for y in f(ys)]
-
-
-    tikzexport = True
-
+def create_plot(data, step_size, tikzexport):
     plt.figure(dpi=1200)
+    mismatch_count = list(map(lambda x: len(x.mismatches), data))
+
+    histbins = np.arange(min(mismatch_count) - 0.5, max(mismatch_count) + 1.5)
+    weights = np.ones_like(mismatch_count) / len(mismatch_count)
+    plt.xlim(min(histbins), max(histbins))
+    plt.xticks(np.arange(min(mismatch_count), max(mismatch_count)+1, step=1))
+    plt.xlabel('$x$')
+    plt.ylabel('$\hat{Pr}[X=x]$')
+    plt.hist(mismatch_count, bins=histbins, align='mid', weights=weights)
+    plt.tight_layout()
+    if tikzexport:
+        tikzplotlib.save(f'../tex/fig_meteor_stats_mismatch_count_{step_size}.tikz')
+    else:
+        plt.show()
+    plt.clf()
+
+
+def visualize_mismatches(data):
+    mismatches = list(flat_map(lambda x: x.mismatches, data))
+    mismatches = [(len(tokenization[1]), '||'.join(tokenization[1]), '||'.join(tokenization[3])) for tokenization in mismatches]
+    mismatches = '\\\\\n'.join([x[1] + ' & ' + x[2] for x in mismatches])
+    return mismatches
+
+
+if __name__ == '__main__':
+    tikzexport = False
     for step_size in [128, 1024]:
+        print(f'generating {step_size} bytes stats')
         fname = f'meteor_statistics_{step_size}.pickle'
         data = ld(fname)
-        print(f'generating {step_size} bytes stats')
         data_w_coding = list(filter(lambda x: x.coding == 'arithmetic', data))
         if not data_w_coding:
-            continue
-        # print(data)
-        mismatched_codings = list(filter(lambda x: len(x.mismatches) > 0, data_w_coding))
-        encoded_tokens = list(map(lambda x: len(x.encoded_tokens), data_w_coding))
-        mismatch_count = list(map(lambda x: len(x.mismatches), data_w_coding))
-        mismatch_prob = len(list(filter(lambda x: x == 0, mismatch_count))) / len(mismatch_count)
-        print(mismatch_prob)
-        mismatch_rate = list(filter(lambda x: x != float('inf'), map(lambda x: x[0] / x[1] if x[1] != 0 else 0,
-                                                                     zip(encoded_tokens, mismatch_count))))
-        avg_mismatch_rate = [sum(encoded_tokens) / sum(mismatch_count) for _ in range(len(encoded_tokens))]
-        mismatches = list(flat_map(lambda x: x.mismatches, data_w_coding))
-        mismatch_len = list(map(lambda x: len(x[3]), mismatches))
-        bits_per_word = list(flat_map(lambda x: x.stats['encoded_bits_in_output'], data_w_coding))
-        avg_bits = np.mean(bits_per_word)
-        avg_bits_per_word = [avg_bits for i in range(len(bits_per_word))]
-        kl = list(map(lambda x: 1 / x.stats['kl'], data_w_coding))
-        ppl = list(map(lambda x: 1 / x.stats['ppl'], data_w_coding))
-
-        histbins = np.arange(min(mismatch_count) - 0.5, max(mismatch_count) + 1.5)
-        weights = np.ones_like(mismatch_count) / len(mismatch_count)
-        plt.xlim(min(histbins), max(histbins))
-        plt.xticks(np.arange(min(mismatch_count), max(mismatch_count)+1, step=1))
-        plt.xlabel('$x$')
-        plt.ylabel('$\hat{Pr}[X=x]$')
-        plt.hist(mismatch_count, bins=histbins, align='mid', weights=weights)
-        plt.tight_layout()
-        if tikzexport:
-            tikzplotlib.save(f'../tex/fig_meteor_stats_mismatch_count_{step_size}.tikz')
-        else:
-            plt.show()
-        plt.clf()
-        """
-        # plt.annotate('%E' % avg_mismatch_rate[-1], (len(avg_mismatch_rate), avg_mismatch_rate[-1]))
-        # plt.plot(avg_mismatch_rate, label="avg mismatch rate")
-        # xaxis = plt.gca()
-        # xaxis.set_visible(False)
-        plt.hist(encoded_tokens, bins=np.arange(min(encoded_tokens) - 0.5, max(encoded_tokens) + 1.5),
-                 label='encoded tokens')
-        plt.tight_layout()
-        plt.legend()
-        if tikzexport:
-            tikzplotlib.save(f'../tex/fig_meteor_stats_encoded_tokens_{step_size}.tikz')
-        else:
-            plt.show()
-        plt.clf()
-
-        plt.hist(mismatch_len, bins=np.arange(min(mismatch_len) - 0.5, max(mismatch_len) + 1.5),
-                 label='mismatch lengths')
-        plt.tight_layout()
-        plt.legend()
-        if tikzexport:
-            tikzplotlib.save(f'../tex/fig_meteor_stats_mismatch_length_{step_size}.tikz')
-        else:
-            plt.show()
-        plt.clf()
-        """
+            raise 'no data'
+        #create_plot(data_w_coding, step_size, tikzexport)
+        mismatches = visualize_mismatches(data_w_coding)
+        m = max(mismatches, key=lambda m: m[0])
+        print(m)
+        print(visualize_mismatches(data_w_coding))
