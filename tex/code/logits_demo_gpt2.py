23e2f35362344c7109241ceac453d89ab6d584b1
Jeremy Boy <github@jboy.eu>
2022-09-26 00:28:47 +0200

    expand -t 2

--- tex/code/logits_demo_gpt2.py
+++ tex/code/logits_demo_gpt2.py
@@ -12,27 +12,27 @@ past = None
 max_len = 128
 temperature = 0.7
 for _ in range(max_len):
-	# get model result for next token
-	result = model(prev.unsqueeze(0), past_key_values=past)
-	logits = result.logits
-	past = result.past_key_values
-	# preprocess logits: sort, convert to double, scale by temperature
-	logits, indices = logits[0, -1, :].sort(descending=True)
-	logits = logits.double()
-	logits_temp = logits / temperature
-	# apply the softmax function to convert logits to probabilities
-	probs_temp = F.softmax(logits_temp, dim=0)
-	# accumulate probabilites
-	cum_probs = probs_temp.cumsum(0)
-	# sample from probabilites
-	sample = np.random.random_sample()
-	selection = (cum_probs > sample).nonzero()[0].item()
+  # get model result for next token
+  result = model(prev.unsqueeze(0), past_key_values=past)
+  logits = result.logits
+  past = result.past_key_values
+  # preprocess logits: sort, convert to double, scale by temperature
+  logits, indices = logits[0, -1, :].sort(descending=True)
+  logits = logits.double()
+  logits_temp = logits / temperature
+  # apply the softmax function to convert logits to probabilities
+  probs_temp = F.softmax(logits_temp, dim=0)
+  # accumulate probabilites
+  cum_probs = probs_temp.cumsum(0)
+  # sample from probabilites
+  sample = np.random.random_sample()
+  selection = (cum_probs > sample).nonzero()[0].item()
 
-	# append selected token to output
-	prev = indices[selection].view(1)
-	output = torch.cat((output, prev))
-	# stop if endoftext was sampled
-	if prev == tokenizer.encoder['<|endoftext|>']:
-		break
+  # append selected token to output
+  prev = indices[selection].view(1)
+  output = torch.cat((output, prev))
+  # stop if endoftext was sampled
+  if prev == tokenizer.encoder['<|endoftext|>']:
+    break
 
 print(tokenizer.decode(output, skip_special_tokens=True))
