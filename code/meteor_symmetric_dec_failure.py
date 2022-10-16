13a9aef28a31a7409305f0f8b984e1b69b446d96
Jeremy Boy <github@jboy.eu>
2022-10-17 01:18:12 +0200

    cleanup

--- code/meteor_symmetric_dec_failure.py
+++ code/meteor_symmetric_dec_failure.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from coder import MeteorCoder
 from util import get_model
@@ -7,33 +8,17 @@ from util import get_model
 def main():
     model_name = 'gpt2-medium'
     device = 'cpu'
-
-    print('get model')
     logging.basicConfig(level=logging.DEBUG)
     enc, model = get_model(model_name=model_name, device=device)
-    print('done')
-
     coder = MeteorCoder(enc, model, device)
 
-    # Constants for HMAC-DRBG -- MUST CHANGE FOR SECURE IMPLEMENTATION
     key = b'\xb9^\x03\xa1\xd0\x1b0O\x11\xdc\xf2\xbc\x84N_\xd3\xcb\xedA%;\x05\x06\x87`\x04 {,*\x10\xe2\xd8\x9c\x1a@\xe950\xbf\xcf\xaa\xeeT\xe6j\xe0H\xd2\xd2\xa56a[\n\x81\xaf\xe7\x92\x888w\xd5\xb6'
-    nonce = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-    chosen_context = "Despite a long history of research and wide-spread applications to censorship resistant systems, practical steganographic systems capable of embedding messages into realistic communication distributions, like text, do not exist.\n\n"
-    message_text = "Hello world"
-    # key = os.urandom(64)
-    # nonce = os.urandom(64)
-    import time
-    start = time.time()
-    x, enc_tokens, stats = coder.encode_message(message_text, chosen_context, key, nonce, coding='arithmetic')
-    end = time.time()
-    print("Encode took {:.02f} s".format(end - start))
-    start = time.time()
-    y, dec_tokens = coder.decode_message(x, chosen_context, key, nonce, coding='arithmetic')
-    end = time.time()
-    print("Decode took {:.02f} s".format(end - start))
-    print(enc_tokens)
-    print(dec_tokens)
-    print('Decode("%s") = "%s" != "%s"' % (x.replace('\n', '\\n'), y.replace('\n', '\\n'), message_text))
+    history = "Despite a long history of research and wide-spread applications to censorship resistant systems, practical steganographic systems capable of embedding messages into realistic communication distributions, like text, do not exist.\n\n"
+    message = "Hello world"
+
+    stegotext, _, _ = coder.encode_message(message, history, key, b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', coding='arithmetic')
+    message_recovered, _ = coder.decode_message(stegotext, history, key, b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', coding='arithmetic')
+    print('Decode("%s") = "%s" != "%s"' % (stegotext.replace('\n', '\\n'), message_recovered.replace('\n', '\\n'), message))
 
 
 if __name__ == '__main__':
